757a1a06932f9177ae45fbab0d4c870e4c775ee2
manish d souza <manishdsouza2308@gmail.com>
2020-03-29 19:32:49 +0530

    Add project files.

---
+++ ZetraApi/Controllers/AccountsController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Zetra.Controllers
+{
+    [ApiController]
+    public class AccountController : Controller
+    {
+        private UserManager<IdentityUser> _usermanger;
+        private SignInManager<IdentityUser> _signinmanager;
+
+        public AccountController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
+        {
+            _usermanger = userManager;
+            _signinmanager = signInManager;
+        }
+        // GET api/values
+        [Route("api/accounts")]
+        public async Task<ActionResult> Register()
+        {
+            Stream request =  Request.Body;
+            StreamReader streamReader = new StreamReader(request);
+            var json = streamReader.ReadToEndAsync();
+            return Ok(json);
+            
+
+            //var newUser = new IdentityUser { Email = email, PasswordHash = password, UserName = username };
+            //var result = await _usermanger.CreateAsync(newUser, password);
+
+            //if (result.Succeeded)
+            //{
+            //    await _signinmanager.SignInAsync(newUser, true);
+            //    return Ok("Signed In");
+            //}
+            //return Ok(result.Errors);
+        }
+    }
+}
---
+++ ZetraApi/DbContext/UsersContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Zetra.DbContext
+{
+    public class UsersContext : IdentityDbContext
+    {
+        public UsersContext(DbContextOptions<UsersContext> options) : base(options)
+        {
+
+        }
+    }
+}
